f841bfc0a5bc27965f4776d139d3652a77b6c650
DaniTM06 <74857094+DaniTM06@users.noreply.github.com>
2021-04-07 21:00:39 +0200

    Add files via upload

---
+++ PRACTICA4/BusquedaRuta.m
@@ -0,0 +1,23 @@
+function (COSTES) = devuelve (coste, ruta)
+final= 1;
+inicio = 1;
+coste=0;
+Matriz=G;
+A=size(G) %G es la matriz de costes
+array=zeros(1, A);
+for(i=[1:A]) 
+  if (Matriz(i,7)== 0)
+            i=i+1;
+            j=i;
+  end
+  if(Matriz(i,7 != 0)
+            for(j=[1:A])
+                      if(Matriz(j,7)==0)
+                                j=j+1;         
+                      end
+                      
+  
+  ruta=[7 i
+  
+                    
+                              
