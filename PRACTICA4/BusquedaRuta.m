28d498befc96ad01c88e1e399652d4d2794f7e84
DaniTM06 <74857094+DaniTM06@users.noreply.github.com>
2021-04-14 20:52:15 +0200

    Add files via upload

--- PRACTICA4/BusquedaRuta.m
+++ PRACTICA4/BusquedaRuta.m
@@ -5,19 +5,46 @@ coste=0;
 Matriz=G;
 A=size(G) %G es la matriz de costes
 array=zeros(1, A);
+K=1;
+encontrado=0;
+final=0;
+columna=1;
+
 for(i=[1:A]) 
-  if (Matriz(i,7)== 0)
-            i=i+1;
-            j=i;
+  if (Matriz(i,7)=! 0)
+         Introducimos ese valor en la posicion Resultado[K]=Matriz(j,ant)
+         ant=i;
+         encontrado=1;
   end
-  if(Matriz(i,7 != 0)
-            for(j=[1:A])
-                      if(Matriz(j,7)==0)
-                                j=j+1;         
-                      end
+  %Primera iteracion i=4, j=1, ant=4
+  
+  
+  while(encontrado==1)
+    while(columna=! 7])                         %  c=1               c=2 !!        
+          if(Matriz(j, ant) == 0) %1ª iteracion (j=1 ant=4)--> (j=2 ant=4) --> (j=2 ant=1) --> (j=1 ant=1) -->--> (j=2 ant=1)
+                    j = j+1; %(j=2) 
+          else
+               
+          if(ant==1)
+                     final=1;  
+          end
+          
+                    Introducimos el valor en la posicion Resultado(K)=Matriz(j,ant)
+                    ant=j; %1ª iteracion (ant=2) --> (ant=1)
+                    j=1;
+          end
+          
+          
+          if(final==1)
+                      K=K+1;
+                      ant=memoria+1;
                       
-  
-  ruta=[7 i
-  
+          end
+                    if(columna==7)
+                               encontrado=0;             
+                    end
+      end
+  end
+end
                     
                               
