5fef9035cc86deace5a5501c32e256057e63ff29
DaniTM06 <74857094+DaniTM06@users.noreply.github.com>
2021-04-22 17:09:09 +0200

    Add files via upload

--- PRACTICA4/BusquedaRuta.m
+++ PRACTICA4/BusquedaRuta.m
@@ -6,13 +6,20 @@ Matriz=G;
 A=size(G) %G es la matriz de costes
 array=zeros(1, A);
 K=1;
+L=1;
 encontrado=0;
 final=0;
 columna=1;
+MatrizAux=Matriz;
+%Cada vez que introduzcamos un numero en Resultado, L (la columna) debe
+%avanzar
+%Cada vez que terminemos un posible camino, K aumenta uno (Fila nueva)
 
 for(i=[1:A]) 
-  if (Matriz(i,7)=! 0)
-         Introducimos ese valor en la posicion Resultado[K]=Matriz(j,ant)
+  if (MatrizAux(i,7)=! 0)
+         Resultado(K,L)=A;%Introducimos el INICIO Resultado[K]=A;
+         L=L+1;
+         Resultado(K,L)=MatrizAux(i,A)%Introducimos ese valor en la posicion Resultado[K]=Matriz(j,ant);
          ant=i;
          encontrado=1;
   end
@@ -20,31 +27,28 @@ for(i=[1:A])
   
   
   while(encontrado==1)
-    while(columna=! 7])                         %  c=1               c=2 !!        
-          if(Matriz(j, ant) == 0) %1ª iteracion (j=1 ant=4)--> (j=2 ant=4) --> (j=2 ant=1) --> (j=1 ant=1) --> (j=2 ant=1)
+    while(columna<=A])                         %  c=1               c=2 !!        
+          if(MatrizAux(j, ant) == 0) %1ª iteracion (j=1 ant=4)--> (j=2 ant=4) --> (j=2 ant=1) --> (j=1 ant=1) --> (j=2 ant=1)
                     j = j+1; %(j=2) 
           else
-               
-          if(ant==1)
-                     final=1;  
-          end
-          
-                    Introducimos el valor en la posicion Resultado(K)=Matriz(j,ant)
+                    Resultado(K,L)=MatrizAux(j,ant); %Introducimos el valor en la posicion Resultado(K)=Matriz(j,ant)
+                    L=L+1;
+                    MatrizAux(j,ant)=0;
                     ant=j; %1ª iteracion (ant=2) --> (ant=1)
                     j=1;
           end
-          
-          
-          if(final==1)
-                      K=K+1;
-                      ant=memoria+1;
-                      
+          if(ant==1 && j==1)
+                    Columna=8;
           end
-                    if(columna==7)
-                               encontrado=0;             
-                    end
+          
+          Columna=Columna+1; %C=2 - C=3 - C=4 - 
+          end
+    if(columna>7)
+              K=K+1;
+              encontrado=0;
+    end
+          
       end
   end
-end
                     
                               
