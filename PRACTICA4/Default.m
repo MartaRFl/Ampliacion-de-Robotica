3184875559206ede5536848ffc360fc81e4c5979
DaniTM06 <74857094+DaniTM06@users.noreply.github.com>
2021-05-30 13:25:44 +0200

    Delete PRACTICA4 directory

--- PRACTICA4/Default.m
+++
@@ -1,9 +0,0 @@
-function [ predeterminado ] = Default(G)
-[f,c]=size(G);
-if(f==c)
- predeterminado=ones(f,3); %Generación del vector
- for(i=[1:f])
- predeterminado(i,1)=i; %Adición de puntos
- end
- predeterminado(:,2:3)=predeterminado(:,2:3)*inf; %Adición de costes iniciales
-end
--- PRACTICA4/VectorInfinito.m
+++
@@ -1,8 +0,0 @@
-function [ predeterminado ] = Default(G)
-[f,c]=size(G);
-if(f==c)
- predeterminado=ones(f,3);
- for(i=[1:f])
- predeterminado(i,1)=i;
- end
- predeterminado(:,2:3)=predeterminado(:,2:3)*inf;
